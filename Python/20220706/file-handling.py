e4da130e5e241f41961eaa02be0fc5211250a957
Mike Mendez <michael.dez@gmail.com>
2022-07-07 14:59:45 -0600

    july 6 and 7 assignments + kops get ip script

--- Python/20220706/file-handling.py
+++ Python/20220706/file-handling.py
@@ -1,6 +1,8 @@
 #! /usr/bin/python3
 import os
-""" Write number of lines from equal to count from inFile to outFile """
+
+""" Write number of lines equal to count from inFile to outFile """
+
 
 def writeLines(count, inFile, outFile):
     try:
@@ -9,19 +11,19 @@ def writeLines(count, inFile, outFile):
         allLines = file.readlines()
         outLines = allLines[0:count]
         # print number of lines
-        print("Total number of lines is {0}" .format(len(allLines)))
+        print("Total number of lines is {0}".format(len(allLines)))
         # print("outLines length: {0}" .format(len(outLines))) # I dont understand how list[x:y] works ???
         file2.writelines(outLines)
-        print("Output written to {0}" .format(file2.name))
-                
+        print("Output written to {0}".format(file2.name))
 
     except OSError as err:
         print("There was a small problem. Hope this helps.")
-        print("error string: {0}" .format(err.strerror))
+        print("error string: {0}".format(err.strerror))
     except Exception as err:
         print("I have no idea what you did.")
         print(err)
     finally:
         file.close()
+
 
 writeLines(20, "SampleLogFile.txt", "mikeExerciseOutput.txt")
